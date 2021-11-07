f24990491a69e6d5cee4c7e348ed914a15e6e959
[ltrujillog] <[lorerociotrujillo@gmail.com]>
2021-11-06 22:57:06 -0500

    version 1.0.0 de la aplicación pedidos

---
+++ src/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from './ping.controller';
+export * from './pedido-persona.controller';
+export * from './persona-pedido.controller';
+export * from './pedido-producto.controller';
+export * from './controllerpersona.controller';
+export * from './controllerpedido.controller';
+export * from './controllerproducto.controller';
